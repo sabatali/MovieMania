f1d7e2f0b235055c5cbb796448c85ab70bf4431b
sabatali <maliksabatali@gmail.com>
2024-03-10 20:22:19 +0500

    update Main file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './App/Store.js'
+import { store } from '../src/App/store.js'
 import { BardContextProvider } from './Context/Context.jsx'
 
 
